60f1e21214294de221b55c5f2bf28345f436652a
DuTh <78890029+VFool@users.noreply.github.com>
2025-03-14 16:22:41 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -13,6 +13,9 @@
 # 添加 fancontrol 提供风扇控制支持
 ./scripts/feeds update fancontrol && ./scripts/feeds install -a -f -p fancontrol
 
+# 添加UA2F
+git clone --depth=1 https://github.com/EOYOHOO/UA2F.git package/UA2F
+
 # 修改 argon 为默认主题
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 
