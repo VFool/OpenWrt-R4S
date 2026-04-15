928907474c7767060a90abfd625b67b94a989854
VFool <78890029+VFool@users.noreply.github.com>
2026-04-15 13:51:41 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,8 +15,8 @@
 
 # Add a feed source
 
-#添加风扇控制
-echo 'src-git fancontrol https://github.com/Kazagumo/luci-app-fancontrol.git' >>feeds.conf.default
+#添加风扇控制程序
+echo 'src-git fancontrol https://github.com/DHDAXCW/dhdaxcw-app' >>feeds.conf.default
 
 #添加Turbe Acc
 #curl -sSL https://raw.githubusercontent.com/chenmozhijin/turboacc/luci/add_turboacc.sh -o add_turboacc.sh && bash add_turboacc.sh --no-sfe
