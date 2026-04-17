3b87fb4ffa82e231208b518db0668d54ef674a93
VFool <78890029+VFool@users.noreply.github.com>
2026-04-17 19:52:02 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -7,6 +7,3 @@
 
 #添加风扇控制程序
 echo 'src-git fancontrol https://github.com/DHDAXCW/dhdaxcw-app' >>feeds.conf.default
-
-#添加Turbe Acc
-#curl -sSL https://raw.githubusercontent.com/chenmozhijin/turboacc/luci/add_turboacc.sh -o add_turboacc.sh && bash add_turboacc.sh --no-sfe
