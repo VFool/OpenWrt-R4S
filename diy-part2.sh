d0e8ae9418ec89beb8876ffd1bafe4de323aa748
VFool <78890029+VFool@users.noreply.github.com>
2026-04-17 19:55:39 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-# Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+# 进入 OpenWrt 源码根目录
+cd openwrt
 
-# Modify default theme
-#sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
-
-# Modify hostname
-#sed -i 's/OpenWrt/P3TERX-Router/g' package/base-files/files/bin/config_generate
-
-# TTYD免登录
+# --- TTYD 免登录配置 ---
 sed -i 's|/bin/login|/bin/login -f root|g' feeds/packages/utils/ttyd/files/ttyd.config
+
+# --- 添加 turboacc 加速插件 ---
+curl -sSL https://raw.githubusercontent.com/chenmozhijin/turboacc/luci/add_turboacc.sh -o add_turboacc.sh && bash add_turboacc.sh --no-sfe
+
+# 返回工作目录
+cd $GITHUB_WORKSPACE
