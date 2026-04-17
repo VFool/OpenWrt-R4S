dcb63b02450569132116c2aad0dfee6d64b8c17f
VFool <78890029+VFool@users.noreply.github.com>
2026-04-17 20:15:43 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -8,6 +8,5 @@ sed -i 's|/bin/login|/bin/login -f root|g' feeds/packages/utils/ttyd/files/ttyd.
 
 # --- 添加 turboacc 加速插件 ---
 curl -sSL https://raw.githubusercontent.com/chenmozhijin/turboacc/luci/add_turboacc.sh -o add_turboacc.sh && bash add_turboacc.sh --no-sfe
-
 # 返回工作目录
 cd $GITHUB_WORKSPACE
