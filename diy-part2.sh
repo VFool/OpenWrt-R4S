8197743937639e88473bdb66190a23137fe7a134
VFool <78890029+VFool@users.noreply.github.com>
2026-04-18 16:02:29 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -7,6 +7,5 @@ cd openwrt
 sed -i 's|/bin/login|/bin/login -f root|g' feeds/packages/utils/ttyd/files/ttyd.config
 
 # --- 添加 turboacc 加速插件 ---
-# curl -sSL https://raw.githubusercontent.com/chenmozhijin/turboacc/luci/add_turboacc.sh -o add_turboacc.sh && bash add_turboacc.sh --no-sfe
-# 返回工作目录
+curl -sSL https://raw.githubusercontent.com/mufeng05/turboacc/main/add_turboacc.sh -o add_turboacc.sh && bash add_turboacc.sh
 cd $GITHUB_WORKSPACE
