3bbbfc1db1d6cfbef9316c6041d992e9fbe9e8d6
VFool <78890029+VFool@users.noreply.github.com>
2026-07-20 17:06:11 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -8,3 +8,9 @@ sed -i 's|/bin/login|/bin/login -f root|g' feeds/packages/utils/ttyd/files/ttyd.
 
 # --- 添加 turboacc 加速插件 ---
 curl -sSL https://raw.githubusercontent.com/mufeng05/turboacc/main/add_turboacc.sh -o add_turboacc.sh && bash add_turboacc.sh
+
+# ---从分支名提取版本号---
+BRANCH_VERSION=$(echo "$REPO_BRANCH" | sed 's/^openwrt-//')
+[ -z "$BRANCH_VERSION" ] && BRANCH_VERSION=$(date +%Y%m%d)
+echo "CONFIG_VERSION_NUMBER=\"$BRANCH_VERSION\"" >> .config
+echo "CONFIG_VERSION_RELEASE=\"r$(date +%Y%m%d)-$BRANCH_VERSION\"" >> .config
